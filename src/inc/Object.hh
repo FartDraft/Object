8b1bf3e86f408dd3e17db7b217b909ab515840df
”FartDraft” <fartdraft@gmail.com>
2023-12-27 03:09:01 +0300

    Go to sleep

--- src/inc/Object.hh
+++ src/inc/Object.hh
@@ -1,16 +1,52 @@
+#include <sstream>
 #include "DoublyCircularLinkedList.hh"
 #include "Fraction.hh"
 
 struct Object {
-    virtual std::string name(void) = 0;
+    virtual std::string name(void) const = 0;
+    virtual size_t id(void) const = 0;
+    virtual Object* copy(void) const = 0;
+    virtual void from_string(std::string s) = 0;
+    virtual std::string to_string(void) const = 0;
+
+    bool
+    operator==(const Object& other) const {
+        return typeid(*this) == typeid(other);
+    }
+
+    size_t
+    operator+(const Object& other) const {
+        return this->id() + other.id();
+    }
 };
 
 class FractionDB : public Object, public Fraction {
     using Fraction::Fraction;
 
     std::string
-    name(void) {
+    name(void) const override {
         return "Fraction";
+    }
+
+    size_t
+    id(void) const override {
+        return typeid(*this).hash_code();
+    }
+
+    FractionDB*
+    copy(void) const override {
+        FractionDB* new_ptr = new FractionDB(*this);
+        return new_ptr;
+    }
+
+    void
+    from_string(std::string s) override {
+        this->assign(s);
+    }
+
+    std::string
+    to_string(void) const override {
+        return std::string(*this);
     }
 };
 
@@ -19,7 +55,53 @@ class DoublyCircularLinkedListDB : public Object, public DoublyCircularLinkedLis
     using DoublyCircularLinkedList<T>::DoublyCircularLinkedList;
 
     std::string
-    name(void) {
+    name(void) const override {
         return "DoublyCircularLinkedList";
+    }
+
+    size_t
+    id(void) const override {
+        return typeid(*this).hash_code();
+    }
+
+    DoublyCircularLinkedListDB<T>*
+    copy(void) const override {
+        DoublyCircularLinkedListDB<T>* new_ptr = new DoublyCircularLinkedListDB<T>(*this);
+        return new_ptr;
+    }
+
+    void
+    from_string(std::string s) override {
+        DoublyCircularLinkedListDB<std::string> new_;
+        std::string tmp = "";
+        for (char c : s) {
+            if (c == ' ') {
+                new_.push_back(tmp);
+                tmp = "";
+            } else {
+                tmp += c;
+            }
+        }
+    }
+
+    std::string
+    to_string(void) const override {
+        struct node {
+            node* prev;
+            T value;
+            node* next;
+        };
+
+        std::stringstream result;
+
+        result << "Head <-> ";
+        if (this->head != nullptr) {
+            result << this->head->value << " <-> ";
+            for (node* curr = (node*)this->head->next; curr != (node*)this->head; curr = curr->next) {
+                result << curr->value << " <-> ";
+            }
+        }
+        result << "Head";
+        return result.str();
     }
 };
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "inc/DoublyCircularLinkedList.hh"
 #include "inc/Fraction.hh"
 #include "inc/Object.hh"
 
@@ -8,13 +7,32 @@ main() {
     FractionDB fraction = 11.0 / 5;
     int arr[] = {1, 2, 3, 4, 5};
     DoublyCircularLinkedListDB<int> list{arr, 5};
+    FractionDB fraction2 = 8.0 / 20;
+    DoublyCircularLinkedListDB list2{list};
+    list2.sort(false);
 
     std::cout << "fraction: " << fraction << std::endl << "list: " << list << std::endl;
+    std::cout << "fraction2: " << fraction2 << std::endl << "list2: " << list2 << std::endl;
 
-    Object* objects[] = {&fraction, &list};
+    Object* objects[] = {&fraction, &list, &fraction2, &list2};
+
+    std::cout << (*objects[0] == *objects[3]) << std::endl;
+    std::cout << (*objects[0] == *objects[2]) << std::endl;
+    std::cout << (*objects[0] + *objects[2]) << std::endl;
+
+    Object* new_obj = objects[0]->copy();
+    std::cout << *(FractionDB*)new_obj << std::endl;
+
+    FractionDB fraction3;
+    Object* new_obj2 = &fraction3;
+    new_obj2->from_string("3 14/100");
+    std::cout << *(FractionDB*)new_obj2 << std::endl;
+    std::cout << new_obj2->to_string() << std::endl;
 
     for (Object* object : objects) {
         std::cout << "Name: " << object->name() << std::endl;
+        std::cout << "Id: " << object->id() << std::endl;
+        std::cout << "To string: " << object->to_string() << std::endl << std::endl;
     }
 
     return 0;
